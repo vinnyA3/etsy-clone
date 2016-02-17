d64b07545ad63d590305ad6384f1d123aa78ea9d
vinnya3 <vinaceto@yahoo.com>
2016-02-17 12:28:06 -0500

    injected Products service, and added new functionality to scope

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,11 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope,Products) {
+  $scope.products = Products.getAllProducts();
+  console.log($scope.products);
+})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Chats, Products) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -15,8 +18,15 @@ angular.module('starter.controllers', [])
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
-})
 
+  //dummy var for image
+  $scope.image = '../img/max.png';
+  $scope.submit = function(product){
+    console.log('button was clicked', product);
+    Products.saveProduct(product,$scope.image);
+  };
+
+})
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
